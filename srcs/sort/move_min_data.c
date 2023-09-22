28ff8661a59ba2a7fbc69bc3730b657798770a05
Kaishiro Mitsui <kamitsui@student.42tokyo.jp>
2023-09-22 14:52:25 +0900

    Add function (move_min_data.c)

---
+++ srcs/sort/move_min_data.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move_min_data.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 14:25:27 by kamitsui          #+#    #+#             */
+/*   Updated: 2023/09/22 14:51:54 by kamitsui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "instruct.h"
+
+// Only call by partition.c
+void	move_min_data(t_stack *stack_a, t_stack *stack_b, int count)
+{
+	i = 0;
+	while (i++ < count)
+		instruct_rrx(stack_b);
+	i = 0;
+	while (i++ < count)
+	{
+		instruct_px(stack_a, stack_b);
+		instruct_rx(stack_a);
+	}
+}
