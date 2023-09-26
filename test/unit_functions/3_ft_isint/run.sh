0192efec2b8fc254a56fdb578333803ca4ee38bf
Kaishiro Mitsui <kamitsui@student.42tokyo.jp>
2023-09-26 21:19:24 +0900

    Remove Unnecessary files

--- test/unit_functions/3_ft_isint/run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-A_OUT=./a.out
-
-printf -- "---- test code ----\n"
-cat srcs/test_ft_isint.c
-printf -- "-------------------\n"
-$A_OUT
--- test/unit_functions/4_partition/run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-A_OUT=./a.out
-
-printf -- "---- test code ----\n"
-cat srcs/test_partition.c
-
-printf -- "-------------------\n"
-$A_OUT
